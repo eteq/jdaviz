385046e78543c20ae0891f57cfe545b197d07bc0
Erik Tollerud <erik.tollerud@gmail.com>
2019-09-05 18:32:21 -0400

    page

--- README.md
+++
@@ -1,6 +0,0 @@
-# jdaviz
-
-A repository for developing astronomical data analysis tools in the Jupyter
-platform. Depending on your point of view, can be "JWST Data Analysis
-Visualization Tools", "Jupyter Data Analysis Visualization", or "Jupyter Data
-analysis for Astronomy with glue-Viz".
---
+++ _static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.1.dev92+g1c68e6d.d20190905',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
